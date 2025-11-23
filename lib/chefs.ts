451818fc40812d346a81c19971133ac1222e2f29
Kyran Sinha <kyransinha@Kyrans-MacBook-Pro.local>
2025-11-23 06:41:10 -0600

    added images

--- lib/chefs.ts
+++ lib/chefs.ts
@@ -3,6 +3,7 @@ export const CHEFS = {
     id: "gordon",
     name: "Gordon Ramsay",
     voiceId: "e605a2a42b0a44ccb7af2e42e1676c92", // your existing
+    image: require("../assets/gordon.jpeg"),
     prompt: `
       You are Gordon Ramsay.
       You MUST respond in Gordon Ramsay's tone:
@@ -18,6 +19,7 @@ export const CHEFS = {
     id: "mario",
     name: "Mario",
     voiceId: "dcb361299bf540fe897b57494ed4b26b",
+    image: require("../assets/mario.jpg"),
     prompt: `
       You are Mario from the Super Mario Bros. franchise.
       Speak friendly, upbeat, and energetic.
@@ -30,6 +32,7 @@ export const CHEFS = {
     id: "guy",
     name: "Guy Fieri",
     voiceId: "8bffd5a802ea48df8502e8e30cf48c3a",
+    image: require("../assets/guy.webp"),
     prompt: `
       flamboyand, energetic and fun, entertaining
       keep it 1-3 sentences max    `,
@@ -39,6 +42,7 @@ export const CHEFS = {
     id: "Anthony",
     name: "Anthony Bourdain",
     voiceId: "31437b4203d74c9a9b97fffd8bdb9c47",
+    image: require("../assets/anthony.jpg"),
     prompt: `
       Adventurous, candid, gritty, empathetic, rebellious, introspective, witty, fearless, worldly, raw, contemplative,Nonchalant, cool, sardonic, laid-back, world-weary, dry, effortless
       keep it 1-3 sentences max  ` ,
